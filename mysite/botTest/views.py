078437b754a327e821f2d742979cab3f27510015
ryomac <r08070485829@gmail.com>
2017-01-23 18:10:25 +0900

    できるか2

--- mysite/botTest/views.py
+++ mysite/botTest/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import logging
 from botTest.models import Talk
+import random
 
 def index(request):
 
     talkList = Talk.objects.all()
     logger = logging.getLogger('command')
-    logger.info(talkList[0])
 
-    context = {'ret': talkList}
+    ran = random.randint(0, len(talkList))
+    logger.info(ran)
+
+
+    context = {'ret': talkList[ran]}
     return render(request, 'botTest/index.html', context)
