c91c7c7850de28157a85903b5c42adf1aaf0b88c
ryomac <r08070485829@gmail.com>
2017-07-11 23:17:41 +0900

    aaa

--- mysite/botTest/views.py
+++ mysite/botTest/views.py
@@ -39,12 +39,9 @@ def ajaxFunc(request):
     logger = logging.getLogger('command')
 
     ran = random.randint(1, len(talkList)) - 1
-    #tl = request.POST['talk']
-    #logger.info(tl)
 
     lis = list()
     lis.append(talkList[ran].content)
-#    lis.append("aaaaaa")
 
     ret1 = talkList[ran].content
     logger.info('ajaxFunc--start--')
@@ -53,14 +50,9 @@ def ajaxFunc(request):
     ret2 = talkList[ran].content
     logger.info(ret2)
 
-#    logger.info(isinstance(ret2.content, str))
-#    logger.info(isinstance(ret2.content, Talk))
-
-#    context = {'ret1': ret1, 'ret2' : ret2}
-    context = {'ret1': 'diaid', 'ret2' : 'ret2'}
+    context = {'ret1': ret1, 'ret2' : ret2}
     logger.info(context)
     logger.info(isinstance(context, dict))
     data = json.dumps(context)
-    logger.info('isinstance(context, dict)')
 
     return HttpResponse(data, content_type='application/json')
