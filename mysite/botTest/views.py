c1c81b10b15e9e82e308f280beb92eb8082f21f6
ryomac <r08070485829@gmail.com>
2017-07-11 22:56:33 +0900

    aaa

--- mysite/botTest/views.py
+++ mysite/botTest/views.py
@@ -29,6 +29,33 @@ def index(request):
 #    logger.info(isinstance(ret2.content, str))
 #    logger.info(isinstance(ret2.content, Talk))
 
+    context = {'ret1': ret1, 'ret2' : ret2}
+
+    return render(request, 'botTest/index.html', context)
+
+def ajaxFunc(request):
+
+    talkList = Talk.objects.all()
+    logger = logging.getLogger('command')
+
+    ran = random.randint(1, len(talkList)) - 1
+    #tl = request.POST['talk']
+    #logger.info(tl)
+
+    lis = list()
+    lis.append(talkList[ran].content)
+#    lis.append("aaaaaa")
+
+    ret1 = talkList[ran].content
+    logger.info('ajaxFunc--start--')
+
+    ran = random.randint(1, len(talkList)) - 1
+    ret2 = talkList[ran].content
+    logger.info(ret2)
+
+#    logger.info(isinstance(ret2.content, str))
+#    logger.info(isinstance(ret2.content, Talk))
+
 #    context = {'ret1': ret1, 'ret2' : ret2}
     context = {'ret1': 'diaid', 'ret2' : 'ret2'}
     logger.info(context)
