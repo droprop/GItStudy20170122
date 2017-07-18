15feca41a50ebb154b57f721e49ac855933fae90
ryomac <r08070485829@gmail.com>
2017-07-19 08:45:33 +0900

    gitacp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'mysite.middleware.BasicAuthMiddleware',
+    'django.middleware.middleware.BasicAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
