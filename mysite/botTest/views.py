d9b6a6a1afe90edfc2cbf93c676af9d3de3586f3
ryomac <r08070485829@gmail.com>
2017-07-18 20:46:22 +0900

    gitacp

--- mysite/botTest/views.py
+++ mysite/botTest/views.py
@@ -1,36 +1,16 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
-import logging
+from django.shortcuts import render
 from botTest.models import Talk
-import random
+
+import logging
 import json
 
 def index(request):
 
     logger = logging.getLogger('command')
 
-#    talkList = Talk.objects.all()
-#    ran = random.randint(1, len(talkList)) - 1
-    #tl = request.POST['talk']
-    #logger.info(tl)
-
-#    lis = list()
-#    lis.append(talkList[ran].content)
-#    lis.append("aaaaaa")
-
-#    ret1 = talkList[ran].content
-#    logger.info(ret1)
-
-#    ran = random.randint(1, len(talkList)) - 1
-#    ret2 = talkList[ran].content
-#    logger.info(ret2)
-
-#    logger.info(isinstance(ret2.content, str))
-#    logger.info(isinstance(ret2.content, Talk))
-
     retTalk = Talk.objects.filter(name='ryo').order_by('?')[0]
-    context = {'ret1': retTalk.content}
+    context = {'retContent': retTalk.content}
     return render(request, 'botTest/index.html', context)
 
 def ajaxFunc(request):
@@ -38,19 +18,7 @@ def ajaxFunc(request):
     retTalk = Talk.objects.filter(name='ryo').order_by('?')[0]
     logger = logging.getLogger('command')
 
-#    ran = random.randint(1, len(talkList)) - 1
-
-#    lis = list()
-#    lis.append(talkList[ran].content)
-
-#    ret1 = talkList[ran].content
-#    logger.info('ajaxFunc--start--')
-
-#    ran = random.randint(1, len(talkList)) - 1
-#    ret2 = talkList[ran].content
-#    logger.info(ret1)
-
-    context = {'ret1': retTalk.content}
+    context = {'retContent': retTalk.content}
     logger.info(context)
     logger.info(isinstance(context, dict))
     data = json.dumps(context)
