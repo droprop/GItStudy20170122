2a95f26110ca36e8bcf6e0220ad94286b7b73da5
ryomac <r08070485829@gmail.com>
2017-07-23 17:53:03 +0900

    gitacp

--- mysite/botTest/views.py
+++ mysite/botTest/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from botTest.models import Talk
-
+from user_agents import parse
 import logging
 import json
 
 def index(request):
 
     logger = logging.getLogger('command')
+    logger.info(request.META['HTTP_USER_AGETN'])
+
+    agent = request.META['HTTP_USER_AGETN']
+
+    logger.info(parse(agent))
+
+    isSP = parse(agent).isMobile
+    logger.info(isSP)
+
 
     retTalk = Talk.objects.filter(name='ryo').order_by('?')[0]
     context = {'retContent': retTalk.content}
     return render(request, 'botTest/index.html', context)
+
 
 def ajaxFunc(request):
 
