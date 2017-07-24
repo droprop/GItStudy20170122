2dd41209023c65ef6fc2f26e8bb8764b92108e31
ryomac <r08070485829@gmail.com>
2017-07-25 08:52:32 +0900

    gitacp

--- mysite/botTest/urls.py
+++ mysite/botTest/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^ajax$', views.ajaxFunc, name='ajaxFunc'),
+    url(r'^ryo$', views.index, name='index'),
+    url(r'^ryo/ajax$', views.ajaxFunc, name='ajaxFunc'),
 ]
