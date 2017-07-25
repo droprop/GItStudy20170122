97eef1aceccd6011c70e3ff1969518b70d299e3b
ryomac <r08070485829@gmail.com>
2017-07-25 22:59:28 +0900

    gitacp

--- mysite/botTest/urls.py
+++ mysite/botTest/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^ryo$', views.index, name='index'),
-    url(r'^ryo/ajax$', views.ajaxFunc, name='ajaxFunc'),
+    url(r'^ryo\/ajax$', views.ajaxFunc, name='ajaxFunc'),
 ]
