a0e4c33122e87eeaa9d72a7a6ec9c2d23c652c97
ryomac <r08070485829@gmail.com>
2017-07-25 23:08:16 +0900

    gitacp

--- mysite/botTest/urls.py
+++ mysite/botTest/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^ryo$', views.index, name='index'),
     url(r'^ryo\/ajax$', views.ajaxFunc, name='ajaxFunc'),
+    url(r'^saki$', views.index, name='indexS'),
+    url(r'^saki\/ajax$', views.ajaxFunc, name='ajaxFuncS'),
 ]
--- mysite/botTest/views.py
+++ mysite/botTest/views.py
@@ -35,3 +35,34 @@ def ajaxFunc(request):
     data = json.dumps(context)
 
     return HttpResponse(data, content_type='application/json')
+
+def indexS(request):
+
+    logger = logging.getLogger('command')
+    logger.info('-----request-----')
+    logger.info(request.META['HTTP_USER_AGENT'])
+
+    agent = request.META['HTTP_USER_AGENT']
+
+    logger.info(parse(agent))
+
+    isSP = parse(agent).is_mobile
+    logger.info(isSP)
+
+
+    retTalk = Talk.objects.filter(name='saki').order_by('?')[0]
+    context = {'retContent': retTalk.content, 'isSP' : isSP}
+    return render(request, 'botTest/saki.html', context)
+
+
+def ajaxFuncS(request):
+
+    retTalk = Talk.objects.filter(name='saki').order_by('?')[0]
+    logger = logging.getLogger('command')
+
+    context = {'retContent': retTalk.content}
+    logger.info(context)
+    logger.info(isinstance(context, dict))
+    data = json.dumps(context)
+
+    return HttpResponse(data, content_type='application/json')
