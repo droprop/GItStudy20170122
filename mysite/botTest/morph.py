fbb2532164c40e2a86c8177b9ddd91f8bf78e0db
droprop <rn8418dev@gmail.com>
2018-02-12 23:34:58 +0900

    awd

--- mysite/botTest/morph.py
+++ mysite/botTest/morph.py
@@ -1,6 +1,6 @@
 import re
 from janome.tokenizer import Tokenizer
-
+import logging
 
 TOKENIZER = Tokenizer()
 
@@ -12,4 +12,6 @@ def analyze(text):
 
 def is_keyword(part):
     """品詞partが学習すべきキーワードであるかどうかを真偽値で返す。"""
+    logger.info('1347')
+    logger.info(part)
     return bool(re.match(r'名詞,(一般|代名詞|固有名詞|サ変接続|形容動詞語幹)', part))
