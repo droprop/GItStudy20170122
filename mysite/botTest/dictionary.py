b3a714f2daa14ed4a9c195007f57948108877123
droprop <rn8418dev@gmail.com>
2018-02-12 23:28:48 +0900

    sasad

--- mysite/botTest/dictionary.py
+++ mysite/botTest/dictionary.py
@@ -42,10 +42,6 @@ class Dictionary:
     def study(self, text, parts):
         """ランダム辞書、パターン辞書、テンプレート辞書をメモリに保存する。"""
         #self.study_keyword(text)
-        logger = logging.getLogger('command')
-        logger.info('-------------------------')
-        logger.info(text)
-        logger.info(parts)
         self.study_pattern(text, parts)
         #self.study_template(parts)
 
@@ -73,16 +69,21 @@ class Dictionary:
 
     def study_pattern(self, text, parts):
         """ユーザーの発言textを、形態素partsに基づいてパターン辞書に保存する。"""
+        logger = logging.getLogger('command')
         for word, part in parts:
             if botTest.morph.is_keyword(part):  # 品詞が名詞であれば学習
                 # 単語の重複チェック
                 # 同じ単語で登録されていれば、パターンを追加する
                 # 無ければ新しいパターンを作成する
+                logger.info('1342')
                 duplicated = next((p for p in self._pattern if p['pattern'] == word), None)
                 if duplicated:
+                    logger.info('1343')
                     if not text in duplicated['phrases']:
                         duplicated['phrases'].append(text)
+                        logger.info('1345')
                 else:
+                    logger.info('1344')
                     self._pattern.append({'pattern': word, 'phrases': [text]})
 
     def save(self):
@@ -90,6 +91,9 @@ class Dictionary:
         with open(Dictionary.DICT['keyword'], mode='w', encoding='utf-8') as f:
             f.write('\n'.join(self.keyword))
 
+        logger = logging.getLogger('command')
+        logger.info('1346')
+        logger.info(self._pattern)
         with open(Dictionary.DICT['pattern'], mode='w', encoding='utf-8') as f:
             f.write('\n'.join([Dictionary.pattern_to_line(p) for p in self._pattern]))
 
